31b8b516e5a6686ab57b8d3f81cb1b334452ca7b
djgxp <gael.trebos@gmail.com>
2019-09-02 22:15:13 -0400

    Update Configuration.php
    Fix deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('djgxp_guzzle');
+        $treeBuilder = new TreeBuilder('djgxp_guzzle');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
